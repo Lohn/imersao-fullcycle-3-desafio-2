6dafb5e85d3bac3e6a6c953ccb906d784b10286b
Josemar Lohn <j@lohn.in>
2021-07-09 07:29:49 -0300

    Fix Migration

--- api/src/migrations/1625818702166-create_route_table.ts
+++ api/src/migrations/1625818702166-create_route_table.ts
@@ -5,11 +5,11 @@ export class createRouteTable1625818702166 implements MigrationInterface {
 
     public async up (queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "routes" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "startPosition" character varying NOT NULL, "endPosition" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_76100511cdfa1d013c859f01d8b" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`INSERT INTO "routes" (title, startPosition, endposition) VALUES ("First Route", "-27.5830119,-48.6088232,16", "-27.5830129,-48.6088232,15")`);
-        await queryRunner.query(`INSERT INTO "routes" (title, startPosition, endposition) VALUES ("Second Route", "-25.5830119,-48.6088232,16", "-27.5830129,-48.6088232,15")`);
-        await queryRunner.query(`INSERT INTO "routes" (title, startPosition, endposition) VALUES ("Third Route", "-26.5830119,-48.6088232,16", "-27.5830129,-48.6088232,15")`);
-        await queryRunner.query(`INSERT INTO "routes" (title, startPosition, endposition) VALUES ("Fourth Route", "-23.5830119,-48.6088232,16", "-27.5830129,-48.6088232,15")`);
-        await queryRunner.query(`INSERT INTO "routes" (title, startPosition, endposition) VALUES ("Fifth Route", "-21.5830119,-48.6088232,16", "-27.5830129,-48.6088232,15")`);
+        await queryRunner.query(`INSERT INTO "routes" ("id", "title", "startPosition", "endPosition") VALUES ('853f5cb4-fc59-4c13-a098-3f2ebdb0de89','First Route', '-27.5830119,-48.6088232,16', '-27.5830129,-48.6088232,15')`);
+        await queryRunner.query(`INSERT INTO "routes" ("title", "startPosition", "endPosition") VALUES ('Second Route', '-25.5830119,-48.6088232,16', '-27.5830129,-48.6088232,15')`);
+        await queryRunner.query(`INSERT INTO "routes" ("title", "startPosition", "endPosition") VALUES ('Third Route', '-26.5830119,-48.6088232,16', '-27.5830129,-48.6088232,15')`);
+        await queryRunner.query(`INSERT INTO "routes" ("title", "startPosition", "endPosition") VALUES ('Fourth Route', '-23.5830119,-48.6088232,16', '-27.5830129,-48.6088232,15')`);
+        await queryRunner.query(`INSERT INTO "routes" ("title", "startPosition", "endPosition") VALUES ('Fifth Route', '-21.5830119,-48.6088232,16', '-27.5830129,-48.6088232,15')`);
 
     }
 
