c77a9cabffd1294731e4db6639ea0dc1ec69782c
Josemar Lohn <j@lohn.in>
2021-07-09 07:04:06 -0300

    Test Migration

--- api/src/migrations/1625818702166-create_route_table.ts
+++ api/src/migrations/1625818702166-create_route_table.ts
@@ -5,6 +5,7 @@ export class createRouteTable1625818702166 implements MigrationInterface {
 
     public async up (queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "routes" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "startPosition" character varying NOT NULL, "endPosition" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_76100511cdfa1d013c859f01d8b" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`INSERT INTO "routes" (title, startPosition, endposition) VALUES ("First Route", "-27.5830119,-48.6088232,16", "-27.5830129,-48.6088232,15")`);
     }
 
     public async down (queryRunner: QueryRunner): Promise<void> {
