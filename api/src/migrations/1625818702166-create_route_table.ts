3469ce35a7aad2ca537da36f4a79f68f1168daab
Josemar Lohn <j@lohn.in>
2021-07-10 00:26:13 -0300

    Fix uuid_v4

--- api/src/migrations/1625818702166-create_route_table.ts
+++ api/src/migrations/1625818702166-create_route_table.ts
@@ -4,6 +4,7 @@ export class createRouteTable1625818702166 implements MigrationInterface {
     name = 'createRouteTable1625818702166'
 
     public async up (queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
         await queryRunner.query(`CREATE TABLE "routes" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "startPosition" character varying NOT NULL, "endPosition" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_76100511cdfa1d013c859f01d8b" PRIMARY KEY ("id"))`);
         await queryRunner.query(`INSERT INTO "routes" ("id", "title", "startPosition", "endPosition") VALUES ('853f5cb4-fc59-4c13-a098-3f2ebdb0de89','First Route', '-27.5830119,-48.6088232,16', '-27.5830129,-48.6088232,15')`);
         await queryRunner.query(`INSERT INTO "routes" ("title", "startPosition", "endPosition") VALUES ('Second Route', '-25.5830119,-48.6088232,16', '-27.5830129,-48.6088232,15')`);
